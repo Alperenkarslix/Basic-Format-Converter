54e68b7d61945b972d9745e141fe869e223c9372
Alperenkarslix <alperenkarsliceng@gmail.com>
2022-10-13 17:23:59 +0300

    first commit

---
+++ converter.js
@@ -0,0 +1,22 @@
+function showFileName() {
+    var fil = document.getElementById("myFile");
+    alert(fil.value);
+    
+}
+// var docxConverter = require('docx-pdf');
+// var giris = document.getElementById("dosya").files[0].name; 
+// console.log(giris)
+// function donustur()
+// {
+    
+
+//     docxConverter(''+giris+'.pdf',''+cikis+'.pdf',function(err,result){
+//       if(err){
+//         console.log(err);
+//       }
+//       console.log('result'+result);
+//     });
+// }
+// module.exports = {
+//     donustur,   
+// }
